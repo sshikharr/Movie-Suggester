92a008e8409516bfeec05bb2d72876731765b8f8
sshikharr <shikhardwivedi2019@gmail.com>
2024-07-21 22:38:38 +0530

    Routing update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Movie from './pages/Movie'
-import { RecoilRoot } from 'recoil'
-import WishList from './pages/Wishlist'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Movie from './pages/Movie';
+import { RecoilRoot } from 'recoil';
+import WishList from './pages/Wishlist';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
 function App() {
-  
   return (
     <>
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/home' element={<HomePage/>}/>
-          <Route path='/movie' element={<Movie/>}/>
-          <Route path='/wishlist' element={<WishList/>}/>
-          <Route path='/signin' element={<Signin/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-          <Redirect from="/" to="/signin" />
-        </Routes>
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/movie' element={<Movie />} />
+            <Route path='/wishlist' element={<WishList />} />
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/' element={<Navigate replace to="/signin" />} />
+          </Routes>
+        </BrowserRouter>
       </RecoilRoot>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
